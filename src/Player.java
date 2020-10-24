9e7e4ecfa82f0dcdea5e310b0be81501d58af4bc
ManiukIvan <manuk-ivan@mail.ru>
2020-10-24 21:24:05 +0300

    format

--- src/Player.java
+++ src/Player.java
@@ -2,5 +2,6 @@ import javafx.util.Pair;
 
 public abstract class Player {
     protected int number;
-    public  abstract void makeMove(TicTacToeDesk desk);
+
+    public abstract void makeMove(TicTacToeDesk desk);
 }
