00681c766712662b1fca1a9807b572ccfe66d3c6
ManiukIvan <manuk-ivan@mail.ru>
2020-10-24 21:22:09 +0300

    add:extend Player

---
+++ src/TicTacToeComputerPlayer.java
@@ -0,0 +1,32 @@
+import javafx.util.Pair;
+
+import java.util.Scanner;
+
+import static java.lang.Math.random;
+
+public class TicTacToeComputerPlayer extends Player {
+
+    public TicTacToeComputerPlayer(int computerNumber) {
+        this.number = computerNumber;
+    }
+
+
+    @Override
+    public void makeMove(TicTacToeDesk desk) {
+        boolean isMadeMove = false;
+        while (!isMadeMove) {
+            int x = (int) (random()*desk.getDeskSize());
+            int y = (int) (random()*desk.getDeskSize());
+            try {
+                desk.fillCell(x, y, number);
+                isMadeMove = true;
+            } catch (TicTacToeException e) {
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Computer "+ number;
+    }
+}
