161acdb241186935d17185dabe264c309d5c3055
ManiukIvan <manuk-ivan@mail.ru>
2020-10-24 21:23:15 +0300

    added new game mode Player Vs Player

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,13 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+
 public class TicTacToe {
 
+    private enum GameMode {
+        PlayerVsPlayer,
+        PlayerVsComputer
+    }
+
     private TicTacToeDesk desk;
-    private TicTacToePlayer player1;
-    private TicTacToePlayer player2;
+    private Player player1;
+    private Player player2;
+    private Player computer;
+    private GameMode gameMode;
 
     public TicTacToe(int deskSize) {
         desk = new TicTacToeDesk(deskSize);
+        chooseGameMode();
         player1 = new TicTacToePlayer(1);
-        player2 = new TicTacToePlayer(2);
+        switch (gameMode){
+            case PlayerVsPlayer:
+                player2 =new TicTacToePlayer(2);
+                break;
+            case PlayerVsComputer:
+                player2 =new TicTacToeComputerPlayer(2);
+                break;
+        }
+        computer = new TicTacToeComputerPlayer(3);
 
     }
 
@@ -17,25 +38,53 @@ public class TicTacToe {
             System.out.println(player1.toString() + ":");
             player1.makeMove(desk);
             if (desk.isWinner()) {
+                desk.showDesk();
                 endGame(player1);
-                break;
+                return;
             }
 
             desk.showDesk();
-            System.out.println(player1.toString() + ":");
+            System.out.println(player2.toString() + ":");
             player2.makeMove(desk);
             if (desk.isWinner()) {
+                desk.showDesk();
                 endGame(player2);
-                break;
+                return;
             }
         }
         endGame(null);
 
     }
 
-    public void endGame(TicTacToePlayer winner) {
+    public void chooseGameMode() {
+        System.out.println("Please Chose game mode:\n" +
+                "1 - Player vs Player\n" +
+                "2 - Player vs Computer\n");
+        Scanner in = new Scanner(System.in);
+        while (gameMode == null) {
+            try {
+                int inputNumber = Integer.parseInt(in.next());
+                switch (inputNumber) {
+                    case 1:
+                        gameMode = GameMode.PlayerVsPlayer;
+                        break;
+                    case 2:
+                        gameMode = GameMode.PlayerVsComputer;
+                        break;
+                    default:
+                        System.out.println("Incorrect value.");
+                        break;
+
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter number  1 or 2");
+            }
+        }
+    }
+
+    public void endGame(Player winner) {
         if (winner != null) {
-            System.out.println("Winner is " + player1.toString());
+            System.out.println("Winner is " + winner.toString());
         } else {
             System.out.println("There is no winner, all cells are filled.");
         }
