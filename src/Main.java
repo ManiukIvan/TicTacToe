e8986123b795be1aab1765397bb90f8b48788fc2
ManiukIvan <manuk-ivan@mail.ru>
2020-11-10 21:47:43 +0300

    added final value of deskSize.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
-
+    private final static int deskSize = 3;
     public static void main(String[] args) {
-        TicTacToe ticTacToe = new TicTacToe(3);
+        TicTacToe ticTacToe = new TicTacToe(deskSize);
         ticTacToe.startGame();
     }
 }
