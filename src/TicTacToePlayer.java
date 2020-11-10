ab5cce9ca77ad81f7614d9cd6b1fe8e9def3a09b
ManiukIvan <manuk-ivan@mail.ru>
2020-11-10 21:20:55 +0300

    removed try catch block from makeMove() method and put it in chooseCell() method. also while replaced with do while

--- src/TicTacToePlayer.java
+++ src/TicTacToePlayer.java
@@ -6,45 +6,52 @@ public class TicTacToePlayer extends Player {
     private Scanner in;
 
     public TicTacToePlayer(int playerNumber) {
-        this.number = playerNumber;
+        this.playerNumber = playerNumber;
         in = new Scanner(System.in);
     }
 
-
     @Override
     public void makeMove(TicTacToeDesk desk) {
-        boolean isMadeMove = false;
-        while (!isMadeMove) {
-            Pair<Integer, Integer> xy = chooseCell();
-            try {
-                desk.fillCell(xy.getKey(), xy.getValue(), number);
-                isMadeMove = true;
-            } catch (TicTacToeException e) {
-                System.out.println(e.getMessage());
+        boolean moveIsNotMade = true;
+        do {
+            Pair<Integer, Integer> xy = chooseCell(desk.getDeskSize());
+            int x = xy.getKey();
+            int y = xy.getValue();
+            if (desk.isFreeCell(x, y)) {
+                desk.fillCell(x, y, playerNumber);
+                moveIsNotMade = false;
             }
-        }
+        } while (moveIsNotMade);
     }
 
-    public Pair<Integer, Integer> chooseCell() {
-        int x = -1;
-        int y = -1;
-        while (x == -1 && y == -1) {
+    public Pair<Integer, Integer> chooseCell(int deskSize) {
+        int x;
+        int y;
+        do {
             try {
                 System.out.println("Enter position(x,y) of the cell.");
                 System.out.println("Enter x:");
                 x = Integer.parseInt(in.next());
                 System.out.println("Enter y:");
                 y = Integer.parseInt(in.next());
+                if (!(0 <= x && x <=deskSize  && 0 <= y && y <= deskSize)) {
+                    throw new TicTacToeException("There is no cell with coordinates:(" + x + "," + y + ").");
+                }
             } catch (NumberFormatException e) {
+                System.out.println("Enter integer value");
+            } catch (TicTacToeException e) {
+                System.out.println(e.getMessage());
+            }
+            finally {
                 x = -1;
                 y = -1;
             }
-        }
+        } while (x == -1 && y == -1);
         return new Pair<>(x, y);
     }
 
     @Override
     public String toString() {
-        return "Player " + number;
+        return "Player " + playerNumber;
     }
 }
